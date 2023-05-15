2a98a5b6140f2542a8482cd2d36c26b74a2cdeac
ryan <ryanpark@usa.com>
2023-05-15 22:05:50 +0000

    Edits for redux, will need to fix response from server

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// Redux
+import { setUser } from '../store/actions';
+import { useDispatch } from 'react-redux';
+
+// Styling
+import '../assets/loginform.css'
+
 
 function login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [user, setUser] = useState(null);
+  const [user, isUser] = useState(null);
   const [loginError, setLoginError] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -27,8 +38,11 @@ function login() {
           credentials: 'include'
         })
         const userData = await userResponse.json();
-        setUser(userData.user);
-        setLoginError(false);
+        isUser(userData.user);            // Used to set locally on this component
+        dispatch(setUser(userData.user)); // Used to set in the redux store
+        //setLoginError(false);             // Does not display error message/ will be replaced with redirect to home
+        navigate('/');
+
       } else {
         // Login is bad, print error message to user
         setLoginError(true);
@@ -41,30 +55,32 @@ function login() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <button type="submit">Log in</button>
-      {loginError && <div>Unable to login</div>}
-      {user && <div>Username: {user.username}</div>}
-      {user && <div>Email: {user.email}</div>}
-    </form>
+    <div className="loginbox">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input
+            type="text"
+            id="username"
+            placeholder="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            id="password"
+            placeholder="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Log in</button>
+        {loginError && <div>Unable to login</div>}
+        {user && <div>Username: {user.username}</div>}
+        {user && <div>Email: {user.email}</div>}
+      </form>
+    </div>
   );
 }
 
